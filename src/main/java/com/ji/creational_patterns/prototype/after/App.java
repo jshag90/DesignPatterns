e2d3e1339e79ede36c98af810ae7709faacf8a87
SeonHakJi <jshag90@naver.com>
2023-03-15 23:10:53 +0900

    [Feat] 프로토타입 패턴 적용 후 clone() shallow copy -> deep copy

--- src/main/java/com/ji/creational_patterns/prototype/after/App.java
+++ src/main/java/com/ji/creational_patterns/prototype/after/App.java
@@ -2,7 +2,32 @@ package com.ji.creational_patterns.prototype.after;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        GithubRepository repository = new GithubRepository();
+        repository.setUser("whiteship");
+        repository.setName("live-study");
+
+        GithubIssue githubIssue = new GithubIssue(repository);
+        githubIssue.setId(1);
+        githubIssue.setTitle("1주차 과제: JVM은 무엇이며 자바 코드는 어떻게 실행하는 것인가.");
+
+        String url = githubIssue.getUrl();
+        System.out.println(url);
+
+        GithubIssue clone = (GithubIssue) githubIssue.clone();
+        System.out.println(clone.getUrl());
+
+        repository.setUser("Seonhak");
+
+        //Java는 기본적으로 shallow copy이지만 clone()을 오버라이딩하여 deep copy를 구현할 수 있음
+        System.out.println(clone != githubIssue);
+        System.out.println(clone.equals(githubIssue));
+        System.out.println(clone.getClass() == githubIssue.getClass());
+        System.out.println(clone.getRepository() == githubIssue.getRepository());
+
+        System.out.println(clone.getUrl());
+
 
     }
 }
