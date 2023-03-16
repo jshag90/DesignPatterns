91b156e0a420129bc884eaf871df96174598bdcd
SeonHakJi <jshag90@naver.com>
2023-03-16 22:41:19 +0900

    [Feat] adapter 패턴 적용 전, 다른 객체에 연결을 위한 adapter가 없는 상태까지 코드 구현

---
+++ src/main/java/com/ji/structural_patterns/adapter/before/Account.java
@@ -0,0 +1,34 @@
+package com.ji.structural_patterns.adapter.before;
+
+public class Account {
+
+    private String name;
+
+    private String password;
+
+    private String email;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
