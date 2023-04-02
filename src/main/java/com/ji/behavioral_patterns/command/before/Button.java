6f7b90a3d440f49cf99c31db15b926909c59c5b4
SeonHakJi <jshag90@naver.com>
2023-04-02 16:07:31 +0900

    [Feat] 커맨드(Command) 패턴 적용 전 예시

---
+++ src/main/java/com/ji/behavioral_patterns/command/before/Button.java
@@ -0,0 +1,21 @@
+package com.ji.behavioral_patterns.command.before;
+
+public class Button {
+
+    private Light light;
+
+    public Button(Light light) {
+        this.light = light;
+    }
+
+    public void press(){light.off();}
+
+    public static void main(String[] args) {
+        Button button = new Button(new Light());
+        button.press();
+        button.press();
+        button.press();
+        button.press();
+    }
+
+}
---
+++ src/main/java/com/ji/behavioral_patterns/command/before/MyApp.java
@@ -0,0 +1,22 @@
+package com.ji.behavioral_patterns.command.before;
+
+public class MyApp {
+
+    private Game game;
+
+    public MyApp(Game game) {
+        this.game = game;
+    }
+
+    public void press(){
+        game.start();
+    }
+
+    public static void main(String[] args) {
+        Button button = new Button(new Light());
+        button.press();
+        button.press();
+        button.press();
+        button.press();
+    }
+}
