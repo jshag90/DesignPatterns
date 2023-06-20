8852f528d62ff220b354aa98c0e6f06156bb82f2
seonhakji <jshag1990@gmail.com>
2023-06-20 22:51:21 +0900

    [Feat] 자바에서 인터프리터 패턴 누락 클래스

---
+++ src/main/java/com/ji/behavioral_patterns/interpreter/java/Book.java
@@ -0,0 +1,18 @@
+package com.ji.behavioral_patterns.interpreter.java;
+
+public class Book {
+
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
