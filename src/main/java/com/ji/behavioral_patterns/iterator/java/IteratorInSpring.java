fa387d411e6939dcfa3c2792d29c66ffb7faec44
seonhakji <jshag1990@gmail.com>
2023-06-20 23:13:05 +0900

    [Feat] 자바, 스프링에서 이터레이터 패턴

---
+++ src/main/java/com/ji/behavioral_patterns/iterator/java/IteratorInSpring.java
@@ -0,0 +1,10 @@
+package com.ji.behavioral_patterns.iterator.java;
+
+import org.springframework.util.CompositeIterator;
+
+public class IteratorInSpring {
+
+    public static void main(String[] args) {
+        CompositeIterator iterator;
+    }
+}
