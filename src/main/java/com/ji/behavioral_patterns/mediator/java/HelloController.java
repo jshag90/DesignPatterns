bfe1a8515d40d724b5466745219365d79271b760
seonhakji <jshag1990@gmail.com>
2023-06-21 23:32:26 +0900

    [Feat] 자바 스프링에서 사용하는 중재자 패턴

---
+++ src/main/java/com/ji/behavioral_patterns/mediator/java/HelloController.java
@@ -0,0 +1,13 @@
+package com.ji.behavioral_patterns.mediator.java;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ src/main/java/com/ji/behavioral_patterns/mediator/java/MediatorInSpring.java
@@ -0,0 +1,10 @@
+package com.ji.behavioral_patterns.mediator.java;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MediatorInSpring {
+
+    public static void main(String[] args) {
+        DispatcherServlet dispatcherServlet;
+    }
+}
