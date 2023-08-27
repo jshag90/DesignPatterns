ccf51d90c7b4314b6d23231702158890b019d995
seonhakji <jshag1990@gmail.com>
2023-08-27 17:27:16 +0900

    [Feat] 자바, 스프링에서 사용하는 Visitor 패턴

---
+++ src/main/java/com/ji/behavioral_patterns/visitor/java/VisitorInSpring.java
@@ -0,0 +1,10 @@
+package com.ji.behavioral_patterns.visitor.java;
+
+import org.springframework.beans.factory.config.BeanDefinitionVisitor;
+
+public class VisitorInSpring {
+
+    public static void main(String[] args) {
+        BeanDefinitionVisitor beanDefinitionVisitor;
+    }
+}
